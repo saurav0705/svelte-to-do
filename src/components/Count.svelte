3f45aaf8148cba1f8508f67900aac2e8e0d2efd0
saurav0705 <sauravaggarwal98@gmail.com>
2020-06-04 17:33:08 +0530

    started

---
+++ src/components/Count.svelte
@@ -0,0 +1,21 @@
+<script>
+let count  = 0;
+let double = 0;
+const increment = () => count++;
+const decrement = () => count--;
+$:  double = count*2;
+</script>
+
+<div>
+<h1> count :: {count}</h1>
+<h1> twice :: {double}</h1>
+<button on:click={increment}>⏫</button>
+<button on:click={decrement}>⏬</button>
+</div>
+
+<style>
+button{
+    background-color: transparent;
+    border: none
+}
+</style>
