7a3f93eed36a646189fdc8d236a13c178953a71d
saurav0705 <sauravaggarwal98@gmail.com>
2020-06-11 22:41:33 +0530

    added css

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -21,10 +21,11 @@ import {Link} from "svelte-routing";
         padding: 2vh;
         font-weight: 500;
         text-transform: uppercase;
+        font-size:24px;
         color:white;
         cursor: pointer;
         transition-duration: 200ms;
-        border-bottom: 2px solid lightcoral;
+        // border-bottom: 2px solid lightcoral;
         &:hover{
             background-color: white;
             color: lightcoral;
